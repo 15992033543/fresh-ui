05212cab7acaa266117f5c08e1efb988776cb3d6
huangzhaoping <395342674@qq.com>
2018-04-02 18:30:41 +0800

    1

--- src/js/button.js
+++ src/js/button.js
@@ -5,34 +5,33 @@
 (function ($) {
   class Button {
     constructor (el, option) {
-      this.$el = el
+      this.el = el
       this.option = option
     }
     show () {
-      this.$el.prop('disabled', true)
-      this.option.text = this.$el.html()
-      this.$el.html(this.option.loadingHtml)
+      this.option.text = this.el.html()
+      this.el.prop('disabled', true)
+      this.el.html(this.option.loadingHtml)
     }
     hide () {
-      this.$el.prop('disabled', false)
-      this.$el.html(this.option.text)
+      this.el.prop('disabled', false)
+      this.el.html(this.option.text)
     }
   }
 
   $.fn.loading = function (type, loadingText='加载中...') {
     if (!type) return
     $(this).each((i, e) => {
-      const $el = $(e)
-      if (!$el.is('button')) return
+      const el = $(e)
+      if (!el.is('button')) return
       const loadingHtml = `<i class="icon-btn-loading"></i>${loadingText}`
-      let instance = $el.data('instance')
+      let instance = el.data('instance')
       if (instance) {
         instance.option.loadingHtml = loadingHtml
         instance[type]()
       } else {
-        const option = { loadingHtml }
-        instance = new Button($el, option)
-        $el.data('instance', instance)
+        instance = new Button(el, { loadingHtml })
+        el.data('instance', instance)
         instance[type]()
       }
     })
--- src/js/modal.js
+++ src/js/modal.js
@@ -6,19 +6,20 @@
   class Modal {
     constructor (el) {
       this.el = el
-      this.content = this.el.find('.modal-content')
-      this.content.css('transition-duration', '100ms')
+      // this.content = this.el.find('.modal-content')
+      // this.content.css('transition-duration', '100ms')
     }
     show () {
       this.el.css('display', 'block')
-      this.content.css('transform', 'scale(0.95, 0.95)')
-      this.content[0].offsetWidth // 强制回流，否则transition无效
-      this.content.css('transform', 'scale(1, 1)')
+      // this.content.css('transform', 'scale(0.95, 0.95)')
+      // this.content[0].offsetWidth // 强制回流，否则transition无效
+      // this.content.css('transform', 'scale(1, 1)')
     }
     hide () {
-      this.content.css('transform', 'scale(0.95, 0.95)').animEndListener(() => {
-        this.el.css('display', 'none')
-      }, 100)
+      // this.content.css('transform', 'scale(0.95, 0.95)').animEndListener(() => {
+      //   this.el.css('display', 'none')
+      // }, 100)
+      this.el.css('display', 'none')
     }
   }
 
