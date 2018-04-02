80aa0d70cc0eee3a43096aff6c49ccc1efdb54c5
huangzhaoping <395342674@qq.com>
2018-04-02 16:05:32 +0800

    1

--- src/js/button.js
+++ src/js/button.js
@@ -3,17 +3,38 @@
  * author: hzp
  */
 (function ($) {
-  $.fn.loading = function (type, text='加载中...') {
+  class Button {
+    constructor (el, option) {
+      this.$el = el
+      this.option = option
+    }
+    show () {
+      this.$el.prop('disabled', true)
+      this.option.text = this.$el.html()
+      this.$el.html(this.option.loadingHtml)
+    }
+    hide () {
+      this.$el.prop('disabled', false)
+      this.$el.html(this.option.text)
+    }
+  }
+
+  $.fn.loading = function (type, loadingText='加载中...') {
     if (!type) return
     $(this).each((i, e) => {
       const $el = $(e)
       if (!$el.is('button')) return
-      const btnText = $el.data('btn-text')
-      !btnText && $el.data('btn-text', $el.html())
-      const isShow = type === 'show'
-      const html = isShow ? `<i class="icon-btn-loading"></i>${text}` : btnText
-      $el.html(html)
-      $el.prop('disabled', isShow)
+      const loadingHtml = `<i class="icon-btn-loading"></i>${loadingText}`
+      let instance = $el.data('instance')
+      if (instance) {
+        instance.option.loadingHtml = loadingHtml
+        instance[type]()
+      } else {
+        const option = { loadingHtml }
+        instance = new Button($el, option)
+        $el.data('instance', instance)
+        instance[type]()
+      }
     })
   }
 }(jQuery))
